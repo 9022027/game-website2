99eaba2ee9de637bb837020e4d7216d792a5e5f6
farai <113636611+gutssan@users.noreply.github.com>
2024-01-22 23:02:48 +0100

    comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 /* Author: Farai de Grave */
+
+// Retrieve HTML elements
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("#scoreText");
 const resetBtn = document.querySelector("#resetBtn");
+// Game variables
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 const boardBackground = "lightgreen";
@@ -23,12 +26,13 @@ let snake = [
     {x:unitSize, y:0},
     {x:0, y:0}
 ];
-
+// Add event listeners
 window.addEventListener("keydown", changeDirection);
 resetBtn.addEventListener("click", resetGame);
+// Start the game on page load
 
 gameStart();
-
+// Function to start the game
 function gameStart(){
     running= true;
     scoreText.textContent = score;
@@ -36,52 +40,81 @@ function gameStart(){
     drawFood();
     nextTick();
 };
-function nextTick(){
-    if(running){
-        setTimeout(()=>{
+
+// Function that performs each "tick" of the game
+function nextTick() {
+    if (running) {
+        // Calculate the duration of the timeout (game speed)
+        const timeoutDuration = 112 - (score * 2);
+       // Timeout to execute the next tick
+        setTimeout(() => {
             clearBoard();
             drawFood();
             moveSnake();
             drawSnake();
             checkGameOver();
             nextTick();
-        }, 112);
-    }
-    else{
+        }, timeoutDuration);
+    } else {
+        // Display the end of the game when not playing anymore
         displayGameOver();
     }
 };
+
+// Function to clear the game board
 function clearBoard(){
     ctx.fillStyle = boardBackground;
     ctx.fillRect(0, 0, gameWidth, gameHeight);
 };
-function createFood(){
-    function randomFood(min, max){
-        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
-        return randNum;
+
+// Function to create food
+function createFood() {
+    function randomFood(min, max) {
+        return Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
     }
-    foodX = randomFood(0, gameWidth - unitSize);
-    foodY = randomFood(0, gameWidth - unitSize);
-};
+
+// Keep trying until food is not placed on the snake
+    do {
+        foodX = randomFood(0, gameWidth - unitSize);
+        foodY = randomFood(0, gameHeight - unitSize);
+    } while (isFoodOnSnake());
+   // Check if food is placed on the snake
+    function isFoodOnSnake() {
+        for (let i = 0; i < snake.length; i++) {
+            if (foodX === snake[i].x && foodY === snake[i].y) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// Function to draw food
 function drawFood(){
     ctx.fillStyle = foodColor;
     ctx.fillRect(foodX, foodY, unitSize, unitSize);
 };
+
+// Function to move the snake
 function moveSnake(){
     const head = {x: snake[0].x + xVelocity,
                   y: snake[0].y + yVelocity};
-    
+     // Add the new head to the beginning of the snake
     snake.unshift(head);
-    //if food is eaten
+    
+    // If food is eaten, increase the score and create new food 
     if(snake[0].x == foodX && snake[0].y == foodY){
         score+=1;
         scoreText.textContent = score;
         createFood();
     }
     else{
+        // If no food is eaten, remove the last part of the snake
         snake.pop();
     }     
 };
+
+// Function to draw the snake
 function drawSnake(){
     ctx.fillStyle = snakeColor;
     ctx.strokeStyle = snakeBorder;
@@ -90,37 +123,59 @@ function drawSnake(){
         ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
     })
 };
-function changeDirection(event){
+
+// Function for changing direction when pressing keys
+function changeDirection(event) {
+    const directionChangeCooldown = 150;
+    let lastDirectionChangeTime = 0;
+    const currentTime = Date.now();
+
+    // Check if enough time has passed since the last direction change
+    if (currentTime - lastDirectionChangeTime < directionChangeCooldown) {
+        return;
+    }
+
+    lastDirectionChangeTime = currentTime;
+
+    // Define key codes for directions
     const keyPressed = event.keyCode;
     const LEFT = 37;
     const UP = 38;
     const RIGHT = 39;
     const DOWN = 40;
 
-    const goingUp = (yVelocity == -unitSize);
-    const goingDown = (yVelocity == unitSize);
-    const goingRight = (xVelocity == unitSize);
-    const goingLeft = (xVelocity == -unitSize);
+   // Check the current movement direction
+    const goingUp = yVelocity === -unitSize;
+    const goingDown = yVelocity === unitSize;
+    const goingRight = xVelocity === unitSize;
+    const goingLeft = xVelocity === -unitSize;
 
-    switch(true){
-        case(keyPressed == LEFT && !goingRight):
-            xVelocity = -unitSize;
-            yVelocity = 0;
+    let newDirection;
+
+   // Determine the new direction based on the pressed key
+    switch (true) {
+        case keyPressed === LEFT && !goingRight:
+            newDirection = { x: -unitSize, y: 0 };
             break;
-        case(keyPressed == UP && !goingDown):
-            xVelocity = 0;
-            yVelocity = -unitSize;
+        case keyPressed === UP && !goingDown:
+            newDirection = { x: 0, y: -unitSize };
             break;
-        case(keyPressed == RIGHT && !goingLeft):
-            xVelocity = unitSize;
-            yVelocity = 0;
+        case keyPressed === RIGHT && !goingLeft:
+            newDirection = { x: unitSize, y: 0 };
             break;
-        case(keyPressed == DOWN && !goingUp):
-            xVelocity = 0;
-            yVelocity = unitSize;
+        case keyPressed === DOWN && !goingUp:
+            newDirection = { x: 0, y: unitSize };
             break;
     }
-};
+
+    // Change the direction if a valid new direction is found   
+    if (newDirection && (newDirection.x !== xVelocity || newDirection.y !== yVelocity)) {
+        xVelocity = newDirection.x;
+        yVelocity = newDirection.y;
+    }
+}
+
+// Function to check if the game is over
 function checkGameOver(){
     switch(true){
         case (snake[0].x < 0):
@@ -142,6 +197,8 @@ function checkGameOver(){
         }
     }
 };
+
+// Function to display game over message
 function displayGameOver(){
     ctx.font = "50px MV Boli";
     ctx.fillStyle = "black";
@@ -149,6 +206,8 @@ function displayGameOver(){
     ctx.fillText("Wasted", gameWidth / 2, gameHeight / 2);
     running = false;
 };
+
+//function to reset the game
 function resetGame(){
     score = 0;
     xVelocity = unitSize;
